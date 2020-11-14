45e1b7048acfc15dc108846e334e460ea1a41334
boar1ang <inq.ops@gmail.com>
2020-11-14 16:41:04 -0600

    Final commit.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # PyBank
 This is a repo containing my solution for the second Python challenge.
++ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + 
+
+In this challenge, I was tasked with creating a Python script for analyzing some financial records for a fictitious company. I was given a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses.
+
+
+I had to create a Python script to analyze the records & calculate each of the following:
+
+    The total number of months included in the dataset
+
+    The net total amount of "Profit/Losses" over the entire period
+
+    The average of the changes in "Profit/Losses" over the entire period
+
+    The greatest increase in profits (date and amount) over the entire period
+
+    The greatest decrease in losses (date and amount) over the entire period
+
+The final script displays the results in the terminal and prints the output to an external text file.
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,62 +1,66 @@
 import os
 import csv
-
-def fin_analysis(budget_csv):
-    
-    print(result_rows)
-    
-    
-
+import sys 
 #-------------------------------------------------------
 budget_csv = os.path.join("Resources", "budget_data.csv")
 #Read in the CSV file:
 with open(budget_csv, 'r', newline="") as csvfile_object:
     csvreader = csv.reader(csvfile_object, delimiter=",")
-    print(type(csvreader))
+    #print(type(csvreader))
     header = next(csvreader)
     #Print the Header row
     print(f'\n{header}')
+    #Set total to 0 prior to calculations:
     total = 0
+    #Set months of data to 0 before counting:
     month_count = 0
-    #Initiate an empty list to hold the integers for Profit/Losses:
+    #Initiate an empty list for the dates
+    dates = []
+    #Initiate an empty list to hold the integer values for Profit/Losses:
     numbers = []
     
     #For each row in the csv file, print the data and the running total:
     for row in csvreader:
+        d = (row[0])
         i = int(row[1])
         print(f'{row[0]} |  {i}')
         total = total + (int(row[1]))
         month_count += 1
+        dates.append(d)
         numbers.append(i)
         
     #Calculate greatest incr and decr:
     greatest_incr = max(numbers)
+    dateIndexIncrPos = (numbers.index(greatest_incr))
+    incr_date = dates[dateIndexIncrPos]
+    
     greatest_decr = min(numbers)
+    dateIndexDecrPos = numbers.index(greatest_decr)
+    decr_date = dates[dateIndexDecrPos]
+
     end = sum(numbers)
     initial_value = numbers[0]
     ending_value = numbers[-1]
     avg_change_long = ((ending_value - initial_value)/ending_value)    
     avg_change = round(avg_change_long, 2)           
+    
+    print(f'\nFinancial Analysis\n+ + + + + + + + + + + + + + + + +\n')
     print(f'\nTotal Net Profits/Losses = ${total}\n')  
     print(f'Total Number of Months: {month_count}\n')    
-    print(f'The greatest increase was ${greatest_incr}\n')
-    print(f'The greatest decrease was ${greatest_decr}\n')
-    print(f'The percentage change over the time period was {avg_change}%.')
-    
-result_header = ['Financial Analysis\n+ + + + + + + + + + + + + + + + +\n']
-result_rows = [
-    "\nTotal Net Profits/Losses = ${total}\n",  
-    "Total Number of Months: {month_count}\n",    
-    "The greatest increase was ${greatest_incr}\n",
-    "The greatest decrease was ${greatest_decr}\n"
-        ]
+    print(f'The greatest increase was ${greatest_incr} during {incr_date}\n')
+    print(f'The greatest decrease was ${greatest_decr} during {decr_date}\n')
+    print(f'The percentage change over the time period was {avg_change}%.\n')  
 
 #-------------------------------------------------------------------------
 
-output_file = os.path.join("Resources", "new.csv")
+output_file = os.path.join("Resources", "new.txt")
 
 with open(output_file, 'w') as file_object:
-    csvwriter = csv.writer(file_object)    
-    csvwriter.writerow(result_header)
-    csvwriter.writerows(result_rows)
-   
+    file_object.write(f'This message will be displayed on the screen:\n')
+    
+    print(f'\n+ + + + + + + + + + + + + + + + +\nFinancial Analysis\n+ + + + + + + + + + + + + + + + +\n', file = file_object)
+    print(f'\nTotal Net Profits/Losses = ${total}\n', file = file_object)  
+    print(f'Total Number of Months: {month_count}\n', file = file_object)    
+    print(f'The greatest increase was ${greatest_incr} during {incr_date}\n', file = file_object)
+    print(f'The greatest decrease was ${greatest_decr} during {decr_date}\n', file = file_object)
+    print(f'The percentage change over the time period was {avg_change}%.\n', file = file_object)   
